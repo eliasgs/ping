d586a3597bc304297fc24871ecfc179a55d8fc82
Elias G. Schneevoigt <eliasgs@gmail.com>
2015-09-30 14:51:30 +0200

    Add TLS function

--- ping.go
+++ ping.go
@@ -3,6 +3,7 @@
 package ping
 
 import (
+	"crypto/tls"
 	"net"
 	"time"
 )
@@ -13,6 +14,19 @@ const timeout = 500 * time.Millisecond
 // can be established within 500 ms.
 func TCP(address string) bool {
 	conn, err := net.DialTimeout("tcp", address, timeout)
+	if err != nil {
+		return false
+	}
+	conn.Close()
+	return true
+}
+
+// TLS tries to connect to address over tcp. It returns true if a connection
+// can be established within 500 ms. Use this function instead of TCP() to
+// avoid TLS handshake errors.
+func TLS(address string) bool {
+	d := &net.Dialer{Timeout: timeout}
+	conn, err := tls.DialWithDialer(d, "tcp", address, nil)
 	if err != nil {
 		return false
 	}
--- ping_test.go
+++ ping_test.go
@@ -12,3 +12,14 @@ func TestTCP(t *testing.T) {
 		t.Error("Should be able to connect!")
 	}
 }
+
+func TestTLS(t *testing.T) {
+	canConnect1 := TLS("www.google.com:1")
+	canConnect2 := TLS("www.google.com:443")
+	if canConnect1 {
+		t.Error("Should not be able to connect!")
+	}
+	if !canConnect2 {
+		t.Error("Should be able to connect!")
+	}
+}
